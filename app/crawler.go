13ce1eb74bf8899579ef1eafa87c063f7adabfbd
Korinoken <nikolajs.pelniks@gmail.com>
2017-06-16 12:39:40 +0300

    Big commit of doom

--- app/crawler.go
+++ app/crawler.go
@@ -1,4 +1,4 @@
-package main
+package app
 
 import (
 	"encoding/json"
@@ -7,65 +7,171 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-	_ "strconv"
+	"path"
+	"path/filepath"
+	"sync"
+	"time"
 )
 
-func main() {
-	file, _ := os.Open("conf.json")
-	decoder := json.NewDecoder(file)
-	crawler := TwitchCrawler{}
-	err := decoder.Decode(&crawler)
-	if err != nil {
-		fmt.Println("error:", err)
+type TwitchImageMeta struct {
+	Regex  string
+	Images map[string]interface{}
+}
+type doneFunc func()
+
+func (crawler *TwitchCrawler) spawnWorkers(limit uint, proc doneFunc) (chan<- DownloadImageData, chan error) {
+	c := make(chan DownloadImageData)
+	res := make(chan error)
+
+	for i := uint(0); i < limit; i++ {
+		go func() {
+			for {
+				a, ok := <-c
+				if !ok {
+					proc()
+					return
+				}
+				crawler.DownloadImageFromUrl(a, res)
+			}
+		}()
 	}
-	fmt.Println(crawler)
+	return c, res
+}
+func (crawler *TwitchCrawler) SaveImages(imageList map[string][]TwitchImageMeta) error {
+	var wg sync.WaitGroup
+	wg.Add(20)
+	fn := func() {
+		wg.Done()
+	}
+	c, res := crawler.spawnWorkers(20, fn)
+
+	var imagesBeingProcessed int = 0
+	for channelName, imageMetaSlice := range imageList {
+		err := os.MkdirAll(filepath.FromSlash(crawler.WorkFolder+"/"+channelName), 0777)
+		if err != nil {
+			return err
+		}
+		for _, imageMeta := range imageMetaSlice {
+			imageLink, err := url.Parse(imageMeta.Images["url"].(string))
+			if err != nil {
+				return err
+			}
+			log.Printf("link :%v", imageLink)
+			imageExtension := path.Base(imageLink.Path)
+			imageExtension = filepath.Ext(imageExtension)
+			data := DownloadImageData{ImageUrl: imageMeta.Images["url"].(string),
+				Path:      crawler.WorkFolder + "/" + channelName,
+				ImageName: imageMeta.Regex + imageExtension}
+			c <- data
+			imagesBeingProcessed++
+		}
+	}
+	for ind := imagesBeingProcessed; ind > 0; ind-- {
+		err := <-res
+		if err != nil {
+			log.Printf("Err from channel:%v", err)
+			return err
+		}
+	}
+	close(c)
+	close(res)
+	return nil
 }
 
 type TwitchCrawler struct {
-	ApiKey  string
-	ApiLink string
+	ApiKey            string
+	ApiLink           string
+	ChannelsToProcess map[int]string
+	WorkFolder        string
+	CacheFileName     string
 }
 
-func (crawler *TwitchCrawler) getImageList() (images map[string]string, err error) {
-	client := &http.Client{}
-	url := crawler.ApiLink + `/chat/emoticons`
-	log.Printf("Url : %v",url)
-	req, _ := http.NewRequest("GET", url, nil)
-	req.Header.Set("Client-ID", crawler.ApiKey)
-	req.Header.Set("Accept", "application/vnd.twitchtv.v5+json")
-	response, err := client.Do(req)
-	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("Error while getting list %v", response.Status)
+func (crawler *TwitchCrawler) GetImageList() (images map[string][]TwitchImageMeta, err error) {
+	stat, err := os.Stat(crawler.CacheFileName)
+	var bodyBytes []byte
+	if err != nil || stat.ModTime().AddDate(0, 0, 1).Before(time.Now()) {
+		if err != nil {
+			log.Printf("Making api call, err:%v", err)
+			err = nil
+		} else {
+			log.Printf("Making api call, mod time:%v", stat.ModTime())
+		}
+		client := &http.Client{}
+		apiUrl := crawler.ApiLink + "/chat/emoticons"
+		req, _ := http.NewRequest("GET", apiUrl, nil)
+		req.Header.Set("Client-ID", crawler.ApiKey)
+		req.Header.Set("Accept", "application/vnd.twitchtv.v5+json")
+		response, err := client.Do(req)
+		if response.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("Error while getting list %v", response.Status)
+		}
+		if err != nil {
+			return nil, err
+		}
+		bodyBytes, _ = ioutil.ReadAll(response.Body)
+		file, err := os.Create(crawler.CacheFileName)
+		file.Write(bodyBytes)
+		log.Print("Wrote response to a file")
+	} else {
+		log.Print("Using saved file")
+		bodyBytes, err = ioutil.ReadFile(crawler.CacheFileName)
+		if err != nil {
+			return nil, err
+		}
 	}
-	bodyBytes, _ := ioutil.ReadAll(response.Body)
-	bodyString := string(bodyBytes)
-	log.Printf("Response %v", bodyString)
-	log.Printf("error :%v", err)
-	images = make(map[string]string)
+
+	var f map[string][]interface{}
+	images = make(map[string][]TwitchImageMeta)
+	json.Unmarshal(bodyBytes, &f)
+	//NA marshaling
+	for _, imageMeta := range f["emoticons"] {
+		converted := imageMeta.(map[string]interface{})
+		meta := TwitchImageMeta{}
+		meta.Regex = converted["regex"].(string)
+		f4 := converted["images"].([]interface{})
+		meta.Images = f4[0].(map[string]interface{})
+		name, ok := crawler.ChannelsToProcess[int(meta.Images["emoticon_set"].(float64))]
+		if ok {
+			images[name] = append(images[name], meta)
+		}
+	}
 	return images, err
 }
-func (crawler *TwitchCrawler) downloadImageFromUrl(imageUrl string, imageName string, path string) error {
-	log.Printf("Attempting to get image %s", imageUrl)
-	response, err := http.Get(imageUrl)
+
+type DownloadImageData struct {
+	ImageUrl  string
+	ImageName string
+	Path      string
+}
+
+func (crawler *TwitchCrawler) DownloadImageFromUrl(data DownloadImageData, ch chan error) {
+	log.Printf("Attempting to get image %s", data.ImageUrl)
+	response, err := http.Get(data.ImageUrl)
 	defer response.Body.Close()
 	if err != nil {
 		log.Print(err)
-		return err
+		ch <- err
+		return
 	}
 	if response.StatusCode != http.StatusOK {
-		return fmt.Errorf("Error while getting file %v", response.Status)
+		ch <- fmt.Errorf("Error while getting file %v", response.Status)
+		return
 	}
-	file, err := os.Create(path + `\` + imageName)
+	file, err := os.Create(filepath.FromSlash(data.Path + "/" + data.ImageName))
 	if err != nil {
 		log.Print(err)
-		return err
+		ch <- err
+		return
 	}
 	_, err = io.Copy(file, response.Body)
 	defer file.Close()
 	if err != nil {
 		log.Print(err)
-		return err
+		ch <- err
+		return
 	}
-	return nil
+	ch <- nil
+	return
 }
--- app/crawler_test.go
+++ app/crawler_test.go
@@ -1,107 +1,142 @@
-package main
+package app_test
 
 import (
+	"crypto/rand"
 	"github.com/stretchr/testify/suite"
+	"io/ioutil"
 	"log"
-	"net/http/httptest"
-	"testing"
 	"net/http"
-	"fmt"
-	"io"
+	"net/http/httptest"
+	"net/url"
 	"os"
+	"path"
+	"path/filepath"
 	"strconv"
+	"testing"
+	"twitch-crawler/app"
 )
 
 type CrawlerTestSuite struct {
 	suite.Suite
-	crawler                       TwitchCrawler
-	server                        httptest.Server
-	VariableThatShouldStartAtFive int
-	testDataLocation              string
+	crawler app.TwitchCrawler
+	server  httptest.Server
 }
 
 func (suite *CrawlerTestSuite) SetupSuite() {
 	log.Print("Setting up suite")
-	suite.testDataLocation = `c:\test\images`
 	suite.server = *httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			suite.handleClient(w, r)
 		}))
-	suite.crawler = TwitchCrawler{"key",suite.server.URL}//test config
+	suite.crawler = app.TwitchCrawler{ApiKey: "key",
+		ApiLink:           suite.server.URL + "/images",
+		ChannelsToProcess: map[int]string{10590: "deana"},
+		WorkFolder:        "C:/test/images",
+		CacheFileName:     "twitchApiResponse.json"} //test config
 }
 
 var (
 	testImages = []string{
-		"image1.png",
+		"emoticon-69119-src-0d4b23ce12767ed8-28x28.png",
 		"image2.png",
 	}
 )
 
 func (suite *CrawlerTestSuite) TestDownloadImageFromUrl() {
-	fileEndLocation := suite.testDataLocation + `\download`
+	fileEndLocation := filepath.FromSlash(suite.crawler.WorkFolder)
+	ch := make(chan error, 2)
 	for _, testImage := range testImages {
-		err := suite.crawler.downloadImageFromUrl(suite.server.URL+"?file="+testImage, testImage,
-			fileEndLocation)
-		suite.Assert().Nil(err)
-		_, err = os.Stat(fileEndLocation + `\` + testImage)//check that file exists
+		suite.crawler.DownloadImageFromUrl(app.DownloadImageData{suite.server.URL + "?file=" + testImage, testImage,
+			fileEndLocation}, ch)
+		//err :=  <- ch
+		//suite.Assert().Nil(err)
+		err := os.Remove(filepath.FromSlash(fileEndLocation +
+			"/" +
+			testImage))
 		suite.Assert().Nil(err)
 	}
 }
 func (suite *CrawlerTestSuite) TestGetImageList() {
-	res, err := suite.crawler.getImageList()
-	suite.Assert().NotEmpty(res)
+	//clean call
+	os.Remove(suite.crawler.CacheFileName)
+	res, err := suite.crawler.GetImageList()
 	suite.Assert().Nil(err)
+	suite.Assert().NotEmpty(res, "GetImageList result was empty")
+	_, found := res["deana"]
+	suite.Assert().True(found, "deana message data not found")
+	_, found = res["cmv"]
+	suite.Assert().False(found, "extra channel data found")
+}
+func (suite *CrawlerTestSuite) TestSaveImages() {
+	res, err := suite.crawler.GetImageList()
+	serverLink, _ := url.Parse(suite.server.URL)
+	for _, imageMetaSlice := range res {
+		for _, imageMeta := range imageMetaSlice {
+			imageLink, err := url.Parse(imageMeta.Images["url"].(string))
+			suite.Assert().Nil(err)
+			imageLink.Host = serverLink.Host
+			imageLink.Scheme = serverLink.Scheme
+			query := imageLink.Query()
+			query.Set("file", path.Base(imageLink.Path))
+			imageLink.RawQuery = query.Encode()
+			imageMeta.Images["url"] = imageLink.String()
+			log.Printf("new url : %v", imageLink)
+		}
+	}
+	err = suite.crawler.SaveImages(res)
+	suite.Assert().Nil(err)
+	for channelName, imageMetaSlice := range res {
+		for _, imageMeta := range imageMetaSlice {
+			_, err = os.Stat(filepath.FromSlash(suite.crawler.WorkFolder +
+				"/" + channelName +
+				"/" + imageMeta.Regex +
+				`.png`))
+			suite.Assert().Nil(err)
+		}
+	}
 }
 func TestModelTestSuite(t *testing.T) {
 	suite.Run(t, new(CrawlerTestSuite))
 }
 func (suite *CrawlerTestSuite) TearDownSuite() {
 	log.Print("Closing test server")
+	os.Remove(suite.crawler.WorkFolder)
 	//suite.server.Close()
 }
 
-func (suite *CrawlerTestSuite) handleClient(writer http.ResponseWriter, request *http.Request) {
+func (suite *CrawlerTestSuite) handleClient(response http.ResponseWriter, request *http.Request) {
+	log.Printf("Received request %v", request.URL.Path)
+	if request.URL.Query().Get("file") == "" {
+		handleImageStructureApiRequest(response)
+	} else {
+		handleFileRequest(response, request)
+	}
+}
+func handleFileRequest(response http.ResponseWriter, request *http.Request) {
 	//First of check if Get is set in the URL
 	Filename := request.URL.Query().Get("file")
 	if Filename == "" {
 		//Get not set, send a 400 bad request
-		http.Error(writer, "Get 'file' not specified in url.", 400)
+		http.Error(response, "Get 'file' not specified in url.", 400)
 		return
 	}
-	fmt.Println("Client requests: " + Filename)
-
 	//Check if file exists and open
-	log.Printf("Attempting to get file %v", suite.testDataLocation+`\`+Filename)
-	Openfile, err := os.Open(suite.testDataLocation + `\` + Filename)
-	defer Openfile.Close() //Close after function return
-	if err != nil {
-		//File not found, send 404
-		http.Error(writer, "File not found.", 404)
-		return
-	}
-
-	//File is found, create and send the correct headers
-
-	//Get the Content-Type of the file
-	//Create a buffer to store the header of the file in
-	FileHeader := make([]byte, 512)
-	//Copy the headers into the FileHeader buffer
-	Openfile.Read(FileHeader)
-	//Get content type of file
-	FileContentType := http.DetectContentType(FileHeader)
-
-	//Get the file size
-	FileStat, _ := Openfile.Stat()                     //Get info from file
-	FileSize := strconv.FormatInt(FileStat.Size(), 10) //Get file size as a string
+	log.Printf("Attempting to get file %v", Filename)
+	File := make([]byte, 10000)
+	rand.Read(File)
+	FileSize := strconv.FormatInt(10000, 10) //Get file size as a string
 
 	//Send the headers
-	writer.Header().Set("Content-Disposition", "attachment; filename="+Filename)
-	writer.Header().Set("Content-Type", FileContentType)
-	writer.Header().Set("Content-Length", FileSize)
-
-	//Send the file
-	//We read 512 bytes from the file already so we reset the offset back to 0
-	Openfile.Seek(0, 0)
-	io.Copy(writer, Openfile) //'Copy' the file to the client
+	response.Header().Set("Content-Disposition", "attachment; filename="+Filename)
+	response.Header().Set("Content-Type", "image/png")
+	response.Header().Set("Content-Length", FileSize)
+	response.Write(File)
 	return
+}
+func handleImageStructureApiRequest(response http.ResponseWriter) {
+	body, err := ioutil.ReadFile("testResponse.json")
+	if err != nil {
+		log.Printf("Error while creating test response: %v", err)
+	}
+	response.Write(body)
 }
